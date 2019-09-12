f772b35de2894eb418ad2779ae78d280a604971a
Christin Cech <christin.cech@gmail.com>
2019-09-12 18:40:45 +0000

    Add Validation to Grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-    describe "grams#index action" do
-          it "should successfully show the page" do
-            get :index
-            expect(response).to have_http_status(:success)
+  describe "grams#index action" do
+    it "should successfully show the page" do
+      get :index
+      expect(response).to have_http_status(:success)
     end
   end
 
@@ -23,6 +23,12 @@ RSpec.describe GramsController, type: :controller do
 
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
+    end
+
+    it "should properly deal with validation errors" do
+      post :create, params: { gram: { message: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(Gram.count).to eq 0
     end
   end
 end
